fa2c90204b6e7416ad70e433087d870abca424d5
nambelaas <salmaniseif@gmail.com>
2023-12-24 20:24:31 +0700

    membuat app say hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nambelaas/app-say-hello
+
+go 1.21.5
+
+require github.com/nambelaas/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	go_say_hello "github.com/nambelaas/go-say-hello"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
